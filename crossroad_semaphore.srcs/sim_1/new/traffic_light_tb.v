c5037d3e01f1754fd9856707d0a7b9bebefe289f
DoidoBR <doidobr46@gmail.com>
2024-04-03 15:48:07 -0300

    Refactoring traffic lights to work with Nexys 4 DDR hardware constraints.

--- crossroad_semaphore.srcs/sim_1/new/traffic_light_tb.v
+++ crossroad_semaphore.srcs/sim_1/new/traffic_light_tb.v
@@ -2,38 +2,37 @@
 
 module traffic_light_tb;
 
-    // Inputs
+    //Inputs
     reg [1:0] select;
-    
-    // Outputs
-    wire [4:0] lights;
+
+    //Outputs
+    wire PERSON_G;
+    wire PERSON_R;
+    wire CAR_R;
+    wire CAR_G;
+    wire CAR_B;
     
     // Instantiate the traffic_light module
-    traffic_light dut (
-        .select(select),
-        .lights(lights)
+    traffic_light light_inst (
+        .select(select), // Using only two LSBs of sem0_car_num_reg
+        .PERSON_G(PERSON_G),
+        .PERSON_R(PERSON_R),
+        .CAR_R(CAR_R),
+        .CAR_G(CAR_G),
+        .CAR_B(CAR_B)
     );
 
     // Stimulus
     initial begin
-        $monitor("Time=%0t, Select=%b, Lights=%b", $time, select, lights);
+        $monitor("Time=%0t, Select=%b, Lights=%b", $time, select, PERSON_G,PERSON_R,CAR_R,CAR_G,CAR_B);
         
-        // Test case 1: Select 00
         select = 2'b00;
-        #10; // Wait for 10 time units
-        
-        // Test case 2: Select 01
-        select = 2'b01;
-        #10; // Wait for 10 time units
-        
-        // Test case 3: Select 10
-        select = 2'b10;
-        #10; // Wait for 10 time units
-        
-        // Test case 4: Invalid select
-        select = 2'b11;
-        #10; // Wait for 10 time units
-        
+
+        repeat (10) begin
+            #1; // Wait for 5 nanoseconds
+            select = select + 2'b01; // Increment select value
+        end
+
         // End simulation
         $finish;
     end
--- crossroad_semaphore.srcs/sources_1/new/traffic_light.v
+++ crossroad_semaphore.srcs/sources_1/new/traffic_light.v
@@ -5,24 +5,45 @@
 // 2b'01 - blue;
 // 2b'10 - red;
 
-// Bit meaning:
-// lights[0] = car green_led
-// lights[1] = car blue_led
-// lights[2] = car red_led
-// lights[3] = pedestrian green_led
-// lights[4] = pedestrian red_led
-
 module traffic_light(
     input [1:0] select,
-    output reg[4:0] lights
+    output reg PERSON_G,
+    output reg PERSON_R,
+    output reg CAR_R,
+    output reg CAR_G,
+    output reg CAR_B
     );
 
     always @(*) begin
         case (select)
-            2'b00: lights <= 5'b10001;  //Traffic light green, pedestrian red
-            2'b01: lights <= 5'b10010;  //Traffic light yellow, pedestrian red
-            2'b10: lights <= 5'b01100;  //Traffic light red, pedestrian green
-            default: lights <= 5'b01100; //Traffic light red, pedestrian green (if something goes wrong, defaults to red)
+            2'b00: begin    //Traffic light is Green
+                CAR_R = 0;
+                CAR_G = 1;
+                CAR_B = 0;
+                PERSON_G = 0;
+                PERSON_R = 1;
+            end
+            2'b01: begin    //Traffic light is Yellow
+                CAR_R = 0;
+                CAR_G = 0;
+                CAR_B = 1;
+                PERSON_G = 0;
+                PERSON_R = 1;
+            end
+            2'b10: begin    //Traffic light is Red
+                CAR_R = 1;
+                CAR_G = 0;
+                CAR_B = 0;
+                PERSON_G = 1;
+                PERSON_R = 0;
+            end
+            default: begin  //If an unexpected behaviour occurs, the traffic light is set to red
+                CAR_R = 1;
+                CAR_G = 0;
+                CAR_B = 0;
+                PERSON_G = 1;
+                PERSON_R = 0;
+            end
         endcase
     end    
     
