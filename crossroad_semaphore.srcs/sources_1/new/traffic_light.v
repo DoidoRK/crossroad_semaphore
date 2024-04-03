7eb418cd8167c167abdf06580f079a526fc27f1e
DoidoBR <doidobr46@gmail.com>
2024-04-03 20:12:29 -0300

    Small adjustment in comment pattern

--- crossroad_semaphore.srcs/sources_1/new/traffic_light.v
+++ crossroad_semaphore.srcs/sources_1/new/traffic_light.v
@@ -16,28 +16,28 @@ module traffic_light(
 
     always @(*) begin
         case (select)
-            2'b00: begin    //Traffic light is Green
+            2'b00: begin    //  Traffic light is Green
                 CAR_R = 0;
                 CAR_G = 1;
                 CAR_B = 0;
                 PERSON_G = 0;
                 PERSON_R = 1;
             end
-            2'b01: begin    //Traffic light is Yellow
+            2'b01: begin    //  Traffic light is Yellow
                 CAR_R = 0;
                 CAR_G = 0;
                 CAR_B = 1;
                 PERSON_G = 0;
                 PERSON_R = 1;
             end
-            2'b10: begin    //Traffic light is Red
+            2'b10: begin    //  Traffic light is Red
                 CAR_R = 1;
                 CAR_G = 0;
                 CAR_B = 0;
                 PERSON_G = 1;
                 PERSON_R = 0;
             end
-            default: begin  //If an unexpected behaviour occurs, the traffic light is set to red
+            default: begin  //  If an unexpected behaviour occurs, the traffic light is set to red
                 CAR_R = 1;
                 CAR_G = 0;
                 CAR_B = 0;
