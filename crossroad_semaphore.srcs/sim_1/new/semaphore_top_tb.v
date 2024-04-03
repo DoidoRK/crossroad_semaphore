6d973fd12b659ac2cd5e74d73701671b90335a98
DoidoBR <doidobr46@gmail.com>
2024-04-03 16:05:03 -0300

    Added clock source from hardware, system's hardware is ready.

--- crossroad_semaphore.srcs/sim_1/new/semaphore_top_tb.v
+++ crossroad_semaphore.srcs/sim_1/new/semaphore_top_tb.v
@@ -4,45 +4,35 @@ module semaphore_top_tb;
 
     // Inputs
     reg buff_en;
-    reg [4:0] sem0_car_wire;
-    reg [4:0] sem0_people_wire;
-    reg [4:0] sem1_car_wire;
-    reg [4:0] sem1_people_wire;
+    reg[4:0] sem0_car_wire;
+    reg[4:0] sem0_people_wire;
+    reg[4:0] sem1_car_wire;
+    reg[4:0] sem1_people_wire;
     
     // Outputs
-    wire [4:0] lights;
+
+    // Semaphore 0
+    // LED[0] - PERSON_G
+    // LED[1] - PERSON_R
+    wire LED17_R;
+    wire LED17_G;
+    wire LED17_B;
+
+    // Semaphore 1
+    // LED[2] - PERSON_G
+    // LED[3] - PERSON_R
+    wire LED16_R;
+    wire LED16_G;
+    wire LED16_B;
+    
+    wire[4:0] LED;
     
     // Instantiate the semaphore_top module
-    semaphore_top dut (
-        .buff_en(buff_en),
-        .sem0_car_wire(sem0_car_wire),
-        .sem0_people_wire(sem0_people_wire),
-        .sem1_car_wire(sem1_car_wire),
-        .sem1_people_wire(sem1_people_wire),
-        .lights(lights)
-    );
-
-    always #5 buff_en = ~buff_en; // Invert clock every 5 time units in order to generate 100Mhz clock
+    semaphore_top dut ();
 
     // Stimulus
     initial begin
-        buff_en = 1;
-        // Assign some values to inputs
-        sem0_car_wire = 4'b00000;
-        sem0_people_wire = 4'b00000;
-        sem1_car_wire = 4'b00000;
-        sem1_people_wire = 4'b00000;
-        
-        // Wait for a few time units
-        #10;
-        sem0_car_wire = 4'b00001;
-        #10;
-        sem0_car_wire = 4'b00010;
-        #10;
-        sem0_car_wire = 4'b00011;
-        
-        // End simulation
-        $finish;
+
     end
 
 endmodule // semaphore_top_tb
--- crossroad_semaphore.srcs/sources_1/new/semaphore_top.v
+++ crossroad_semaphore.srcs/sources_1/new/semaphore_top.v
@@ -1,26 +1,34 @@
 `timescale 1ns / 1ps
 
 module semaphore_top (
+    input CLK100MHZ,
+    output wire clk_1hz,
     input wire buff_en,
-    input wire [4:0] sem0_car_wire,
-    input wire [4:0] sem0_people_wire,
-    input wire [4:0] sem1_car_wire,
-    input wire [4:0] sem1_people_wire,
-    output wire [4:0] lights
+    input wire[15:0] SW,
+    output wire[4:0] LED,
+    output wire LED17_R,
+    output wire LED17_G,
+    output wire LED17_B,
+    output wire LED16_R,
+    output wire LED16_G,
+    output wire LED16_B
 );
     // Define registers to store the outputs of traffic_buffer
-    wire [4:0] sem0_car_num_reg;
-    wire [4:0] sem0_people_num_reg;
-    wire [4:0] sem1_car_num_reg;
-    wire [4:0] sem1_people_num_reg;
+    wire[3:0] sem0_car_num_reg;
+    wire[3:0] sem0_people_num_reg;
+    wire[3:0] sem1_car_num_reg;
+    wire[3:0] sem1_people_num_reg;
+
+    wire[1:0] state_traffic_light_0;
+    wire[1:0] state_traffic_light_1;
+
+    clock_divider dut2(.clock_in(CLK100MHZ),.clock_out(clk_1hz));
 
     // Instantiate traffic_buffer module
     traffic_buffer buffer_inst (
         .buff_en(buff_en),
-        .sem0_car_wire(sem0_car_wire),
-        .sem0_people_wire(sem0_people_wire),
-        .sem1_car_wire(sem1_car_wire),
-        .sem1_people_wire(sem1_people_wire),
+
+        .SW(SW),
 
         .sem0_car_num_reg(sem0_car_num_reg),
         .sem0_people_num_reg(sem0_people_num_reg),
@@ -29,10 +37,24 @@ module semaphore_top (
     );
 
     // Instantiate traffic_light module
-    traffic_light light_inst (
-        .select(sem0_car_num_reg[1:0]), // Using only two LSBs of sem0_car_num_reg
-        .lights(lights)
+    traffic_light_0 light_inst (
+        .select(state_traffic_light_0), // Using only two LSBs of sem0_car_num_reg
+        .PERSON_G(LED[0]),
+        .PERSON_R(LED[1]),
+        .CAR_R(LED17_R),
+        .CAR_G(LED17_G),
+        .CAR_B(LED17_B)
     );
 
+    traffic_light_1 light_inst (
+        .select(state_traffic_light_1), // Using only two LSBs of sem1_car_num_reg
+        .PERSON_G(LED[2]),
+        .PERSON_R(LED[3]),
+        .CAR_R(LED16_R),
+        .CAR_G(LED16_G),
+        .CAR_B(LED16_B)
+    );
+
+    // Implement State Machine from here
 
 endmodule // semaphore_top
