3eaf4b029180698827fb94c19d976b757b106be3
DoidoBR <doidobr46@gmail.com>
2024-04-03 14:42:50 -0300

    Refactoring traffic buffer to work with Nexys 4 DDR hardware constraints.

--- crossroad_semaphore.srcs/sim_1/new/traffic_buffer_tb.v
+++ crossroad_semaphore.srcs/sim_1/new/traffic_buffer_tb.v
@@ -4,25 +4,19 @@ module traffic_buffer_tb;
 
     // Inputs
     reg enable;
-    reg [4:0] sem0_car_num;
-    reg [4:0] sem0_people_num;
-    reg [4:0] sem1_car_num;
-    reg [4:0] sem1_people_num;
+    reg [15:0] SW;
     
     // Outputs
-    wire [4:0] sem0_car_num_reg;
-    wire [4:0] sem0_people_num_reg;
-    wire [4:0] sem1_car_num_reg;
-    wire [4:0] sem1_people_num_reg;
+    wire [3:0] sem0_car_num_reg;
+    wire [3:0] sem0_people_num_reg;
+    wire [3:0] sem1_car_num_reg;
+    wire [3:0] sem1_people_num_reg;
     
     // Instantiate the traffic_buffer module
     traffic_buffer dut (
         .buff_en(enable),
         
-        .sem0_car_wire(sem0_car_num),
-        .sem0_people_wire(sem0_people_num),
-        .sem1_car_wire(sem1_car_num),
-        .sem1_people_wire(sem1_people_num),
+        .SW(SW),
 
         .sem0_car_num_reg(sem0_car_num_reg),
         .sem0_people_num_reg(sem0_people_num_reg),
@@ -30,44 +24,27 @@ module traffic_buffer_tb;
         .sem1_people_num_reg(sem1_people_num_reg)
     );
 
+    always #5 enable = ~enable; // Invert clock every 5 time units in order to generate 100Mhz clock
+
     // Stimulus
     initial begin
-        // Enable the buffer
-        enable = 0;
-        
-        // Assign some values to inputs
-        sem0_car_num = 5'b10101;
-        sem0_people_num = 5'b01010;
-        sem1_car_num = 5'b11011;
-        sem1_people_num = 5'b01101;
-        
-        
-        #5; // Wait for a few time units
+        // Initialize signals
         enable = 1;
+        SW = 16'b0000000000000000; // Initialize SW
+        
+        // Change SW values every 5 nanoseconds
+        repeat (16) begin
+            #10; // Wait for 5 nanoseconds
+            SW = SW + 16'b0001000100010001; // Increment SW value
+        end
 
-        
-        #5; // Wait for a few time units
-        enable = 0;
-
-        sem0_car_num = 5'b00000;
-        sem0_people_num = 5'b00001;
-        sem1_car_num = 5'b00010;
-        sem1_people_num = 5'b00011;
-
-        
-        #5; // Wait for a few time units
-        enable = 1;
-        
-        
-        // Display the outputs
-        $display("sem0_car_num_reg=%b", sem0_car_num_reg);
-        $display("sem0_people_num_reg=%b", sem0_people_num_reg);
-        $display("sem1_car_num_reg=%b", sem1_car_num_reg);
-        $display("sem1_people_num_reg=%b", sem1_people_num_reg);
-        
-        #5;
         // End simulation
         $finish;
+    end
+
+    // Display register values at each change
+    always @(posedge enable) begin
+        $display("At time %t, sem0_car_num_reg=%b, sem0_people_num_reg=%b, sem1_car_num_reg=%b, sem1_people_num_reg=%b", $time, sem0_car_num_reg, sem0_people_num_reg, sem1_car_num_reg, sem1_people_num_reg);
     end
 
 endmodule // traffic_buffer_tb
--- crossroad_semaphore.srcs/sources_1/new/traffic_buffer.v
+++ crossroad_semaphore.srcs/sources_1/new/traffic_buffer.v
@@ -3,22 +3,34 @@
 module traffic_buffer(
     input wire buff_en,
 
-    input wire[4:0] sem0_car_wire, 
-    input wire[4:0] sem0_people_wire, 
-    input wire[4:0] sem1_car_wire, 
-    input wire[4:0] sem1_people_wire,
+    input wire[15:0] SW,
 
-    output reg[4:0] sem0_car_num_reg,
-    output reg[4:0] sem0_people_num_reg,
-    output reg[4:0] sem1_car_num_reg,
-    output reg[4:0] sem1_people_num_reg
+    output reg[3:0] sem0_car_num_reg,
+    output reg[3:0] sem0_people_num_reg,
+    output reg[3:0] sem1_car_num_reg,
+    output reg[3:0] sem1_people_num_reg
     );
 
     always @(posedge buff_en) begin
-        sem0_car_num_reg <= sem0_car_wire;
-        sem0_people_num_reg <= sem0_people_wire;
-        sem1_car_num_reg <= sem1_car_wire;
-        sem1_people_num_reg <= sem1_people_wire;
+        sem0_car_num_reg[0] <= SW[0];
+        sem0_car_num_reg[1] <= SW[1];
+        sem0_car_num_reg[2] <= SW[2];
+        sem0_car_num_reg[3] <= SW[3];
+
+        sem0_people_num_reg[0] <= SW[4];
+        sem0_people_num_reg[1] <= SW[5];
+        sem0_people_num_reg[2] <= SW[6];
+        sem0_people_num_reg[3] <= SW[7];
+
+        sem1_car_num_reg[0] <= SW[8];
+        sem1_car_num_reg[1] <= SW[9];
+        sem1_car_num_reg[2] <= SW[10];
+        sem1_car_num_reg[3] <= SW[11];
+
+        sem1_people_num_reg[0] <= SW[12];
+        sem1_people_num_reg[1] <= SW[13];
+        sem1_people_num_reg[2] <= SW[14];
+        sem1_people_num_reg[3] <= SW[15];
     end
 endmodule //traffic_buffer
 
