52ba46538fce45a111cbc1fea7d78acda68d54f5
DoidoBR <doidobr46@gmail.com>
2024-04-03 20:58:24 -0300

    Small adjustments in traffic buffer simulation.

--- crossroad_semaphore.srcs/sim_1/new/traffic_buffer_tb.v
+++ crossroad_semaphore.srcs/sim_1/new/traffic_buffer_tb.v
@@ -3,8 +3,10 @@
 module traffic_buffer_tb;
 
     // Inputs
+    parameter CLK100MHZ = 5;  // Generates clock in a 100Mhz rate
+    reg clk;
     reg enable;
-    reg[15:0] SW;
+    reg[15:0] SW = 16'b0000000000000000;
     
     // Outputs
     wire[3:0] sem0_car_num_reg;
@@ -24,27 +26,28 @@ module traffic_buffer_tb;
         .sem1_people_num_reg(sem1_people_num_reg)
     );
 
-    always #5 enable = ~enable; // Invert clock every 5 time units in order to generate 100Mhz clock
+    always #CLK100MHZ clk = ~clk;
+
+    always #10 enable = ~enable; //Enables buffer read every 10 time units.
 
     // Stimulus
     initial begin
         // Initialize signals
-        enable = 1;
-        SW = 16'b0000000000000000; // Initialize SW
-        
-        // Change SW values every 5 nanoseconds
+        enable = 0;
+        clk = 0;
+
+        // Change SW values every 5 clocks
         repeat (16) begin
-            #10; // Wait for 5 nanoseconds
+            #CLK100MHZ;
+            #CLK100MHZ;
+            #CLK100MHZ;
+            #CLK100MHZ;
+            #CLK100MHZ;
             SW = SW + 16'b0001000100010001; // Increment SW value
         end
 
         // End simulation
         $finish;
-    end
-
-    // Display register values at each change
-    always @(posedge enable) begin
-        $display("At time %t, sem0_car_num_reg=%b, sem0_people_num_reg=%b, sem1_car_num_reg=%b, sem1_people_num_reg=%b", $time, sem0_car_num_reg, sem0_people_num_reg, sem1_car_num_reg, sem1_people_num_reg);
     end
 
 endmodule // traffic_buffer_tb
