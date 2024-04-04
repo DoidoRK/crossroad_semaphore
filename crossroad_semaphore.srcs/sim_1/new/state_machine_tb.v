b62cbdda1edd25c48ce057e1417f432d445bf8be
DoidoBR <doidobr46@gmail.com>
2024-04-03 21:52:26 -0300

    Fixed typos in state machine`s testbench

--- crossroad_semaphore.srcs/sim_1/new/state_machine_tb.v
+++ crossroad_semaphore.srcs/sim_1/new/state_machine_tb.v
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ps
 
-module state_machine_tb
-    wire[7:0] count,
-    reg[3:0] sem0_car_num,
-    reg[3:0] sem0_people_num,
-    reg[3:0] sem1_car_num,
-    reg[3:0] sem1_people_num,
+module state_machine_tb;
+    wire[7:0] count;
+    reg[3:0] sem0_car_num;
+    reg[3:0] sem0_people_num;
+    reg[3:0] sem1_car_num;
+    reg[3:0] sem1_people_num;
 
-    wire rst_count,
-    wire read_en,
-    reg[1:0] state_sem_0,
-    reg[1:0] state_sem_1
+    wire rst_count;
+    wire read_en;
+    reg[1:0] state_sem_0;
+    reg[1:0] state_sem_1;
 
     state_machine dut(
         .count(count),
@@ -25,4 +25,4 @@ module state_machine_tb
         .state_sem_1(state_sem_1)
     );
 
-endmodule
+endmodule   //state_machine_tb
