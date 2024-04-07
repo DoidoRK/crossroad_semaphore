ce7b8c5a37830f30c5135edd3f58bf16f485fa11
DoidoBR <doidobr46@gmail.com>
2024-04-07 14:50:00 -0300

    Semaphore top testbench implemented

--- crossroad_semaphore.srcs/sim_1/new/semaphore_top_tb.v
+++ crossroad_semaphore.srcs/sim_1/new/semaphore_top_tb.v
@@ -1,37 +1,46 @@
 `timescale 1ns / 1ps
 
+// Simulation scenario:
+// Semaphore 0:
+// 2 Car
+// 15 people
+// Semaphore 1:
+// 1 Car
+// 14 people
+
+
 module semaphore_top_tb;
 
   // Inputs
-  reg clk;
-  reg [3:0] sem_0_car_num;
-  reg [3:0] sem_1_car_num;
-  reg [3:0] sem_0_people_num;
-  reg [3:0] sem_1_people_num;
-  wire [1:0] sem_0_state;
-  wire [1:0] sem_1_state;
+  reg CLK100MHZ;
+  reg[15:0] SW;
+  wire[3:0] LED;
+  wire LED17_R;
+  wire LED17_G;
+  wire LED17_B;
+  wire LED16_R;
+  wire LED16_G;
+  wire LED16_B;
 
   //Semaphore instance
   semaphore_top dut (
-    .CLK100MHZ,
-    .SW(),
-    .LED(),
-    .LED17_R(),
-    .LED17_G(),
-    .LED17_B(),
-    .LED16_R(),
-    .LED16_G(),
-    .LED16_B()
+    .CLK100MHZ(CLK100MHZ),
+    .SW(SW),
+    .LED(LED),
+    .LED17_R(LED17_R),
+    .LED17_G(LED17_G),
+    .LED17_B(LED17_B),
+    .LED16_R(LED16_R),
+    .LED16_G(LED16_G),
+    .LED16_B(LED16_B)
   );
+
+  always #1 CLK100MHZ = ~CLK100MHZ; // For simulation purposes
 
   // Clock generation
   initial begin
-    clk = 0;
-    sem_0_people_num = 2;
-    sem_1_people_num = 14;
-    sem_0_car_num = 15;
-    sem_1_car_num = 1;
-    forever #2 clk = ~clk; // Invert clock every 5 time units to generate a 100MHz clock
+    CLK100MHZ = 0;
+    SW = 16'b1110000111110010;
   end
 
 endmodule // semaphore_top_tb
